2f2155040bd22d7919af366b79a6143349765897
jhonguamansanmartin-max <jhonguamansanmartin@gmail.com>
2026-05-30 06:19:08 -0500

    Primer Commit

---
+++ src/main/java/com/krakedev/Taller/Exepciones/ExepcionesApplication.java
@@ -0,0 +1,13 @@
+package com.krakedev.Taller.Exepciones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExepcionesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExepcionesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/krakedev/Taller/Exepciones/ExepcionesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.krakedev.Taller.Exepciones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExepcionesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
